5e8dadff6b6c977de41a8c8df76fdc0db8f8ac5d
pglorio <85982602+pglorio@users.noreply.github.com>
2024-05-20 23:13:47 -0700

    Update configuration_auto.py

--- src/transformers/models/auto/configuration_auto.py
+++ src/transformers/models/auto/configuration_auto.py
@@ -285,6 +285,7 @@ CONFIG_MAPPING_NAMES = OrderedDict(
         ("xmod", "XmodConfig"),
         ("yolos", "YolosConfig"),
         ("yoso", "YosoConfig"),
+        ("zamba", "ZambaConfig"),
     ]
 )
 
@@ -577,6 +578,7 @@ MODEL_NAMES_MAPPING = OrderedDict(
         ("xmod", "X-MOD"),
         ("yolos", "YOLOS"),
         ("yoso", "YOSO"),
+        ("zamba", "Zamba"),
     ]
 )
 
